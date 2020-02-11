dbb1dd7cccd6e914eb5f11bb7737a5f7014577f6
Joelson Reis <joelsondsbreis@gmail.com>
2020-02-11 16:53:04 -0300

    update server.js

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -9,7 +9,7 @@ export class UserService {
   constructor(private http: HttpClient) { }
   baseUrl = 'http://localhost:8080';
 
-  get( ) {
+  get() {
     return this.http.get(this.baseUrl + '/api/getUser');
   }
   post(data) {
